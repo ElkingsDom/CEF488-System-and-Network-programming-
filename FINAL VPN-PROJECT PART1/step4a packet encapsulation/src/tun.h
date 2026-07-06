1e0e9cc67da41aa3c2fd4d081b2dbf5f2ecf3a6c
ElkingsDom <etumaelkings19@gmail.com>
2026-07-06 05:37:50 +0100

    Delete FINAL VPN-PROJECT PART1 directory

--- FINAL VPN-PROJECT PART1/step4a packet encapsulation/src/tun.h
+++
@@ -1,6 +0,0 @@
-#ifndef TUN_H
-#define TUN_H
-
-int tun_alloc(char *dev);
-
-#endif
--- FINAL VPN-PROJECT PART1/step5a encryption/src/crypto.h
+++
@@ -1,7 +0,0 @@
-#ifndef CRYPTO_H
-#define CRYPTO_H
-
-void xor_crypt(unsigned char *data,
-               int len);
-
-#endif
